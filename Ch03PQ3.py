c62bde9dd8f9a7d6e5ddb9afa175c3a2db352ca1
Steve Ayers <steveayers124@gmail.com>
2019-06-25 14:40:42 -0500

    Ch03PQ3.py - investigate string index and slice

---
+++ Ch03PQ3.py
@@ -0,0 +1,20 @@
+# run examples to test my answers to practice questions
+
+spam = 'Four score and seven years is eighty seven years.'
+
+print('spam[5]|' + spam[5])
+print('spam[-3]|' + spam[-3])
+print('spam[0:4] + spam[5]|' + spam[0:4] + spam[5])
+print('spam[-3:-1]|' + spam[-3:-1])
+print('spam[:10]|' + spam[:10])
+print('spam[-5:]|' + spam[-5:])
+print('spam[:]|' + spam[:])
+
+print('now what i\'m curious about')
+print('spam[-1:-3]|' + spam[-1:-3])
+print('spam[-3:-3]|' + spam[-3:-3])
+print('spam[1:1]|' + spam[1:1])
+print('spam[1:2]|' + spam[1:2])
+
+print('and one more thing...')
+val = input('How many\'s four score?: ')
