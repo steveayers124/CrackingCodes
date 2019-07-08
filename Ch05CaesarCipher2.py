1e36b0cdc481dd92bf3545af32457e1b585c7ba5
Steve Ayers <steveayers124@gmail.com>
2019-07-08 15:48:27 -0500

    feat: run programming on end-of-chapter questions

--- Ch05CaesarCipher2.py
+++ Ch05CaesarCipher2.py
@@ -34,9 +34,37 @@ mode = 'encrypt' # Set to either 'encrypt' or 'decrypt'.
 translated = caesarCipher(message, key, mode)
 print(translated)
 pyperclip.copy(translated)
-
 message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
 translated = caesarCipher(message, 13, 'decrypt')
 print(translated)
 pyperclip.copy(translated)
+
+message = '"You can show black is white by argument," said Filby, "but you will never convince me."'
+translated = caesarCipher(message, 8, 'encrypt')
+print(translated)
+pyperclip.copy(translated)
+message = '"gw3EkivE1pw5EjtiksEq1E5pq2mEj7Eizo3umv2,"E1iqlENqtj7,E"j32E7w3E5qttEvm4mzEkwv4qvkmEumH"'
+translated = caesarCipher(message, 8, 'decrypt')
+print(translated)
+pyperclip.copy(translated)
+
+message = '1234567890'
+translated = caesarCipher(message, 21, 'encrypt')
+print(translated)
+pyperclip.copy(translated)
+message = 'HIJKLMNOPQ'
+translated = caesarCipher(message, 21, 'decrypt')
+print(translated)
+pyperclip.copy(translated)
+
+message = 'Kv?uqwpfu?rncwukdng?gpqwijB'
+translated = caesarCipher(message, 2, 'decrypt')
+print(translated)
+pyperclip.copy(translated)
+
+message = 'XCBSw88S18A1S 2SB41SE .8zSEwAS50D5A5x81V'
+translated = caesarCipher(message, 22, 'decrypt')
+print(translated)
+pyperclip.copy(translated)
+
 
