9e61be8b7e7c7180f902560a815e3962401f1b75
Steve Ayers <steveayers124@gmail.com>
2019-09-05 23:39:44 -0500

    TST: record some runs through the transposition cypher

--- Ch07TranspositionEncrypt.py
+++ Ch07TranspositionEncrypt.py
@@ -4,10 +4,15 @@
 import pyperclip
 
 def main():
-    myMessage = 'Common sense is not so common.'
+    # myMessage = 'Common sense is not so common.'
     # myMessage = 'Cenoonommstmme oo snnio. s s c  '
-    myKey = 8
+    # myKey = 8
     # myKey = 4
+
+    myKey = 9
+    myMessage = "Underneath a huge oak tree there was of swine a huge company,"
+    myMessage = "That grunted as they crunched the mast: For that was ripe and fell full"
+    myMessage = "Then they trotted away for the wind grew high: One acorn they left, and no more might you spy."
 
     ciphertext = encryptMessage(myKey, myMessage)
 
